45bd55be29320141e8f33718364a0359a5fb540d
Zdenek Maxa <zden.maxa@gmail.com>
2014-10-17 11:18:54 +0200

    more tests, added chrome tests (needs driver)

--- selenium/test_basic.py
+++ selenium/test_basic.py
@@ -2,9 +2,11 @@
 Selenium tests example to test application running at
 https://xmax-testapp.appspot.com
 
-for py.test, dicovery founds only test_*.py or *_test.py files
+for py.test, discovery founds only test_*.py or *_test.py files
 
 """
+
+import os
 
 import pytest
 
@@ -23,37 +25,56 @@ class TestBasic(object):
 
 class TestApp(TestBasic):
     
-    def setup_method(self, _):
-        # for Chrome ...
-        # WebDriverException: Message: 'ChromeDriver executable needs to be
-        # available in the path.
-        # Please download from
-        # http://chromedriver.storage.googleapis.com/index.html
-        # and read up at http://code.google.com/p/selenium/wiki/ChromeDriver'
-        # cls.browser = webdriver.Chrome()
-        # hint:
-        #return webdriver.Chrome(executable_path=driver,
-        #chrome_options=options)
-        self.browser = webdriver.Firefox()
+    def setup_method(self, method):
+        print "Current working directory: '%s'" % os.getcwd()
+        # according to the name of the test (suffix) distinguish
+        # which type of browser to instantiate
+        if method.__name__.endswith("_chrome"):
+            # for Chrome, when run without its driver:
+            # WebDriverException: Message: 'ChromeDriver executable needs to be
+            # available in the path.
+            # Please download from
+            # http://chromedriver.storage.googleapis.com/index.html
+            # and read up at http://code.google.com/p/selenium/wiki/ChromeDrive
+
+            # if some options, e.g. extensions paths are to be loaded
+            op = webdriver.ChromeOptions()
+            self.browser = webdriver.Chrome(executable_path="./chromedriver-2.11",
+                                            chrome_options=op)
+        else:
+            self.browser = webdriver.Firefox()
 
     def teardown_method(self, _):
         self.browser.quit()
 
-    def test_load(self):
+    def _test_sum(self, x_input, y_input, result):
         self.browser.get(APP_URL)
         print self.browser.title
         # find the element that's name attribute is x, y (input fields)
         x = self.browser.find_element_by_name("x")
         y = self.browser.find_element_by_name("y")
         # simulates typing into the element
-        x.send_keys(30)
-        y.send_keys(35)
+        x.send_keys(x_input)
+        y.send_keys(y_input)
         x.submit()
         try:
-            WebDriverWait(self.browser, 1)
+            # timeout - Number of seconds before timing out
+            timeout = 5
+            WebDriverWait(self.browser, timeout)
         except Exception as ex:
             print(str(ex))
             pytest.fail()
         r = self.browser.find_element_by_name("result")
-        assert r.text == "result: 30 + 35 = 65"
+        assert r.text == result
 
+    def test_sum_correct_input_firefox(self):
+        self._test_sum(30, 35, "result: 30 + 35 = 65")
+
+    def test_sum_wrong_input_firefox(self):
+        self._test_sum(30, "d", "result: 30 + d = error")
+
+    def test_sum_correct_input_chrome(self):
+        self._test_sum(30, 35, "result: 30 + 35 = 65")
+
+    def test_sum_wrong_input_chrome(self):
+        self._test_sum(30, "d", "result: 30 + d = error")

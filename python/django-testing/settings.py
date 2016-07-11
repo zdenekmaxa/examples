f4aee843bb11aa3e04e4070dddcd45b9eb878d0f
Zdenek Maxa <zdenekmaxa@yahoo.co.uk>
2016-07-11 15:33:17 +0200

    minor

--- python/django-testing/settings.py
+++ python/django-testing/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/public/'
 
 # This setting should be defined here
 # There is a test checking if this will be overwritten.

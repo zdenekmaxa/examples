5cb4382304f1c9d478e7dcb25ebd5b1a7e4b6846
Zdenek Maxa <zdenekmaxa@yahoo.co.uk>
2017-04-26 19:23:06 +0200

    chapter 6 started

---
+++ javascript/you_don't_know_js/02-book-this_&_object_prototypes/ch06/02-oloo-style.js
@@ -0,0 +1,23 @@
+var Foo = {
+	init: function(who) {
+		this.me = who;
+	},
+	identify: function() {
+		return "I am " + this.me;
+	}
+};
+
+var Bar = Object.create( Foo );
+
+Bar.speak = function() {
+	console.log( "Hello, " + this.identify() + "." );
+};
+
+var b1 = Object.create( Bar );
+b1.init( "b1" );
+var b2 = Object.create( Bar );
+b2.init( "b2" );
+
+b1.speak();
+b2.speak();
+

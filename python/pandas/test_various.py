181a5e9048b7be4707c2f46f6416eff05a84b4f8
Zdenek Maxa <zdenekmaxa@yahoo.co.uk>
2019-10-03 12:18:45 +0200

    Series.map test

--- python/pandas/test_various.py
+++ python/pandas/test_various.py
@@ -7,6 +7,18 @@ Pandas experiments above the basic level.
 import pandas as pd
 
 
+def test_simple_map_function_1():
+    df = pd.DataFrame(dict(a=[1, 2, 3, 4], b=[5, 6, 7, 8]))
+    mask = df.a.map(lambda x: x > 2)
+    assert df.loc[mask, "a"].to_list() == [3, 4]
+
+
+def test_simple_map_function_1():
+    df = pd.DataFrame(dict(a=['1', '2', 3, 4], b=[5, 6, 7, 8]))
+    mask = df.a.map(type) == str
+    assert df.loc[mask, "a"].to_list() == ['1', '2']
+
+
 def test_define_values_by_dict():
     df = pd.DataFrame(dict(a=[1, 2, 3, 4], b=[5, 6, 7, 8]))
     repl = {1: 11, 2: 22, 4: 55}
@@ -23,8 +35,8 @@ def test_replace_series_values_by_map_file():
     the mapping file is delivered in CSV, old_value, new_value pairs
 
     Want to change values in column 'a'.
-    """
 
+    """
     df = pd.DataFrame(index=["i1", "i2", "i3", "i4"])
     df["a"] = [1, 2, 3, 4]
     df["b"] = [5, 6, 7, 8]
@@ -36,3 +48,4 @@ def test_replace_series_values_by_map_file():
     df.loc[mask, "a"] = df.loc[mask, "_a"]
     df = df.drop(columns=["_a"])
     assert df.loc[["i2", "i4"], "a"].to_list() == [0, 9]
+
